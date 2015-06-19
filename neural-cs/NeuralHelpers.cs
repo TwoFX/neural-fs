6824ad098ae7a995e3e977524bdb23759ddeaeac
Markus Himmel <markus@himmel-villmar.de>
2015-06-19 21:44:59 +0200

    Work on CS code

---
+++ neural-cs/NeuralHelpers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeuralTypes;
+
+namespace NeuralHelpers
+{
+    public class NeuralHelpers
+    {
+        public static IEnumerable<Neuron<TSource, TMeaning>> FromVals<TSource, TMeaning>(int inputNodes, int hiddenLayers, int hiddenNodesPerLayer, int outputNodes, IEnumerable<double> values, IEnumerable<TMeaning> meanings)
+        {
+            var startLayer = new Neuron<TSource, TMeaning>[inputNodes];
+            Queue<double> vals = new Queue<double>(values);
+            for (int i = 0; i < inputNodes; i++)
+            {
+                startLayer[i] = Neuron<TSource, TMeaning>.NewInputNeuron(i);
+            }
+
+            IEnumerable<Neuron<TSource, TMeaning>> currentLayer = startLayer;
+
+            for (int i = 0; i < hiddenLayers; i++)
+            {
+                var nextLayer = new Neuron<TSource, TMeaning>[hiddenNodesPerLayer];
+                for (int j = 0; j < hiddenNodesPerLayer; j++)
+                {
+                    nextLayer[j] = Neuron<TSource, TMeaning>.NewHiddenNeuron(currentLayer.Select(node => new Tuple<Neuron<TSource, TMeaning>, double>(node, vals.Dequeue())).ToArray(), vals.Dequeue());
+                }
+                currentLayer = nextLayer;
+            }
+
+            var outputLayer = new Neuron<TSource, TMeaning>[outputNodes];
+            TMeaning[] ms = meanings.ToArray();
+            for (int i = 0; i < outputNodes; i++)
+            {
+                outputLayer[i] = Neuron<TSource, TMeaning>.NewOutputNeuron(currentLayer.Select(node => new Tuple<Neuron<TSource, TMeaning>, double>(node, vals.Dequeue())).ToArray(), vals.Dequeue(), ms[i]);
+            }
+            return outputLayer;
+        }
+
+        public static double[] toVals<TSource, TMeaning>(IEnumerable<Neuron<TSource, TMeaning>> network)
+        {
+            return null;
+        }
+    }
+}
--- neural-fs/Program.fs
+++ neural-fs/Program.fs
@@ -2,9 +2,11 @@
 
 open System
 open Neural
+open neural_cs
 
 [<EntryPoint>]
 let main argv = 
     printfn "Hello World"
+    NeuralHelpers.FromVals (2, 1, 3, 2, Seq.init 17 (fun x -> x + 1 |> float), ["Test 1"; "Test 2"]) |> ignore
     Console.ReadKey true |> ignore
     0
