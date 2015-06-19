06f897f0e2a0f01981c54be1f5dcf47369e5ddfd
ThreeFx <ben@sjfiedler.de>
2015-06-19 20:37:12 +0200

    Removed and renamed files

--- neural-fs-types/Library1.fs
+++
@@ -1,6 +0,0 @@
-﻿namespace NeuralTypes
-
-type Neuron<'source, 'meaning> =
-    | InputNeuron of id : int
-    | HiddenNeuron of incoming : seq<Neuron<'source, 'meaning> * double> * threshold : double
-    | OutputNeuron of incoming : seq<Neuron<'source, 'meaning> * double> * threshold : double * meaning : 'meaning
---
+++ neural-fs-types/NeuralTypes.fs
@@ -0,0 +1,8 @@
+﻿namespace NeuralTypes
+
+type Neuron<'source, 'meaning> =
+    | InputNeuron of id : int
+    | HiddenNeuron of incoming : seq<Neuron<'source, 'meaning> * double> * threshold : double
+    | OutputNeuron of incoming : seq<Neuron<'source, 'meaning> * double> * threshold : double * meaning : 'meaning
+
+type NeuralNetwork<'source, 'meaning> = seq<Neuron<'source, 'meaning>>
