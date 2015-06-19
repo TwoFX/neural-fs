1a89b145502a1b9827947b1dfa3cce9d7299c301
Markus Himmel <markus@himmel-villmar.de>
2015-06-19 18:03:27 +0200

    Utils vergessen!

---
+++ neural-fs/Util.fs
@@ -0,0 +1,15 @@
+﻿module Util
+
+open System.Collections.Generic
+
+let memoize f =
+    let dict = new Dictionary<'a * 'b,_>()
+    fun a b ->
+        match dict.TryGetValue((a, b)) with
+        | (true, v) -> v
+        | _ ->
+            let temp = f a b
+            dict.Add((a, b), temp)
+            temp
+
+let fix a b = a
